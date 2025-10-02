0f8159c91a8c7510d4d114b499aed4fc98b0e1e5
2459005-dot <2459005@kbu.ac.kr>
2025-10-02 20:07:38 +0900

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 import Landing from './pages/Landing'
+import AuthPanel from './components/AuthPanel'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Landing />} />
-    </Routes>
+    <div className="page">
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/admin/login' element={<AuthPanel />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/components/AuthModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/AuthModal.scss'
+
+const AuthModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthModal
