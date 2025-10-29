915ce7752aa9ad666311c8451d0ac0de33d96d55
2459005-dot <2459005@kbu.ac.kr>
2025-10-29 21:04:07 +0900

    .

--- frontend/src/pages/user/UserPostList.jsx
+++ frontend/src/pages/user/UserPostList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import UserPostItem from "./UserPostItem";
-
+import './style/UserPostList.scss'
 const UserPostList = ({ items = [], loading, onReload, search }) => {
 
     if (!items.length) return <p>게시물이 없습니다.</p>
+
 
     return (
         <div className="post-list">
