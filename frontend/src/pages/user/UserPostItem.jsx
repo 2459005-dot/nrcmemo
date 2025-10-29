dc53a2aa9f73f6ae22dc3dd6eec95981bc1af716
2459005-dot <2459005@kbu.ac.kr>
2025-10-29 21:50:19 +0900

    .

--- frontend/src/pages/user/UserPostItem.jsx
+++ frontend/src/pages/user/UserPostItem.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import './style/UserPostItem.scss'
 const UserPostItem = ({ item }) => {
+
+  const defaultImage = "/images/default-image.png";
   
-    const files =Array.isArray(item.fileUrl)?item.fileUrl:(item?.fileUrl? [item.fileUrl]:[])
-  
-    return (
+  const files = Array.isArray(item.fileUrl)
+    ? item.fileUrl.length > 0
+      ? item.fileUrl
+      : [defaultImage]
+    : item?.fileUrl
+      ? [item.fileUrl]
+      : [defaultImage];
+
+  return (
     <div className="inner post-card">
       <div className="file-card-head">
         {(item?.number ?? "") !== "" && <span>No. {item.number}</span>}
         <h3>{item?.title ?? "제목 없음"}</h3>
+        <time className="right">{item?.updateAt}</time>
       </div>
       <div className="file-card-meta">
         {item?.updateAt && (
-            <time className="file-card-time">{item.updateAt}</time>
+          <time className="file-card-time">{item.updateAt}</time>
         )}
       </div>
       <div className="file-card-details">
-        {item?.content && (
-            <p className="file-card-content">{item.content}</p>
+        {files?.length > 0 && (
+          <div className="file-card-image">
+            {files.map((src, idx) => (
+              <img key={idx} src={src} alt={`file-${idx}`} className="file-card-image" />
+            ))}
+          </div>
         )}
-        {files?.length>0 && (
-            <div className="file-card-image">
-                {files.map((src, idx)=>(
-                    <img key={idx} src={src} alt={`file-${idx}`} className="file-card-image"/>
-                ))}
-            </div>
+        {item?.content && (
+          <p className="file-card-content">{item.content}</p>
         )}
       </div>
     </div>
