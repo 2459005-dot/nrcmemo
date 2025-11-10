f365a238b12d63a08a5a93b4f4ddb18a5f4650e2
2459005-dot <2459005@kbu.ac.kr>
2025-11-10 21:24:41 +0900

    .

--- frontend/src/components/admin/AdminPostsList.jsx
+++ frontend/src/components/admin/AdminPostsList.jsx
@@ -1,40 +1,35 @@
-import React from 'react'
-import { formatYMD } from '../../util/formatYMD'
+import React from "react";
+import { formatYMD } from "../../util/formatYMD";
 
 const AdminPostsList = ({ items = [], onApprove, onReject }) => {
-
   return (
-    <div className='inner'>
-      <header>
-        <span>제목</span>
-        <span>작성자</span>
-        <span>상태</span>
-        <span>업데이트</span>
-        <span>액션</span>
-      </header>
-      <ul className="admin-list-body">
+    <div className="inner">
+      <ul className="admin-list">
+        <li>
+          <span></span>
+          <span>제목</span>
+          <span>작성자</span>
+          <span>상태</span>
+          <span>업데이트</span>
+          <span>액션</span>
+        </li>
         {items.map((it, i) => (
           <li key={it._id}>
             <span className="num">{i + 1}</span>
             <span className="title">{it.title}</span>
-            <span className="user">{it._id}</span>
+            <span className="user">{it._userId}</span>
             <span className="status">{it.status}</span>
             <span className="date">
-              {it.updatedAt ? formatYMD(it.updatedAt) : '-'}
+              {it.updatedAt ? formatYMD(it.updatedAt) : "-"}
             </span>
             <span className="actions">
-              {it.status !== 'approved' && (
-                <button
-                  onClick={() => onApprove(it._id)}
-                  className="btn secondary">
+              {it.status !== "approved" && (
+                <button onClick={() => onApprove(it._id)} className="btn secondary">
                   승인
                 </button>
               )}
-
-              {it.status !== 'rejected' && (
-                <button
-                  onClick={() => onReject(it._id)}
-                  className="btn danger">
+              {it.status !== "rejected" && (
+                <button onClick={() => onReject(it._id)} className="btn danger">
                   거절
                 </button>
               )}
@@ -43,7 +38,7 @@ const AdminPostsList = ({ items = [], onApprove, onReject }) => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPostsList
+export default AdminPostsList;
--- frontend/src/components/admin/AdminUserList.jsx
+++ frontend/src/components/admin/AdminUserList.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { formatYMD } from '../../util/formatYMD'
+import React from "react";
+import { formatYMD } from "../../util/formatYMD";
 
-const AdminUsers = () => {
+const AdminUserList = ({ items = [], onChangeLock, onChangeRole }) => {
   return (
-    <ul>
+    <ul className="admin-list">
       <li>
         <span>id</span>
         <span>email</span>
@@ -19,15 +19,21 @@ const AdminUsers = () => {
           <span>{it.email}</span>
           <span>{it.displayName ?? "-"}</span>
           <span>{it.role}</span>
-          <span>{it.isActive}</span>
+          <span>{it.isActive ? "활성" : "비활성"}</span>
           <span>{it.createdAt ? formatYMD(it.createdAt) : ""}</span>
+          <button className="btn" onClick={() => onChangeRole(it._id, it.role)}>
+            {it.role === 'admin' ? "관리자 해제" : "관리자 지정"}
+          </button>
+          <button className="btn" onClick={() => onChangeLock(it._id, it.isActive)}>
+            {it.isActive ? "활성화" : "비활성화"}
+          </button>
         </li>
       ))}
       {items.length === 0 && (
         <li>사용자 데이터가 없습니다.</li>
       )}
     </ul>
-  )
-}
+  );
+};
 
-export default AdminUsers
+export default AdminUserList;
